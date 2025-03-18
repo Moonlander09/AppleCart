b236d3b53eedbc2af1119bc8d3e5218de0f9463e
Moonlander09 <webdev095@gmail.com>
2025-03-18 14:02:15 +0530

    minor fixes

--- app/products/page.js
+++ app/products/page.js
@@ -15,7 +15,7 @@ export default async function StorePage({ searchParams }) {
 
   return (
     <div className="my-4 mx-4 md:mx-8 flex flex-col gap-6">
-      <h3 className="text-2xl md:text-3xl font-semibold text-center border-b border-gray-100 py-6 md:py-10">
+      <h3 className="text-2xl md:text-3xl font-semibold text-center border-b border-gray-200 py-6 md:py-10">
         Our Products
       </h3>
       
@@ -26,7 +26,7 @@ export default async function StorePage({ searchParams }) {
             <button
               className={`px-4 py-2 rounded-lg transition font-medium text-lg md:text-xl ${
                 category === categoryId
-                  ? "bg-blue-600 text-white"
+                  ? "bg-[#0f345b] text-white"
                   : "bg-gray-200 hover:bg-gray-300"
               }`}
             >
@@ -42,7 +42,7 @@ export default async function StorePage({ searchParams }) {
       </div>
 
       {/* Product Grid */}
-      <div className="pt-6 px-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-10 border-t border-gray-100">
+      <div className="pt-6 px-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-10 border-t border-gray-200">
         {data.map((product) => (
           <div
             key={product._id}
